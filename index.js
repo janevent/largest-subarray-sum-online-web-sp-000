60eacb0c33e2a9d8ae976fbb4ef0b18203364275
Jane Venter <janeanneventer@gmail.com>
2020-09-25 04:48:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function largestSubarraySum(array){
-  let sub = []
-  // for(let i = 0; i < array.length; i++){
-    
-  // }
-  let first = array.slice(0, 2)
-  let sumo = first.reduce((sum, n) => { sum + n }, 0)
-  return sumo
+  let temp_max = 0;
+  let largest_max = 0;
+  for(let i = 0; i < array.length; i++){
+    temp_max = Math.max(0, temp_max += array[i]) 
+    largest_max = Math.max(largest_max, temp_max)
+  }
+  return largest_max
+ // let first = array.slice(0, 2)
+//  let sumo = first.reduce((sum, n) => { sum + n }, 0)
 }
